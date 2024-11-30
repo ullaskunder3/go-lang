eb7ae0dd9dc0e71e1c5010098809b17f4c6bd1b2
Ullas Kunder <ullaskunder3@gmail.com>
2024-11-30 17:34:01 +0530

    update learn variable

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Go!")
+	var testVariable = "this is mr awesome ="
+	testVariableNewWay := "this is mr awesome :="
+
+	fmt.Println(testVariable)
+	fmt.Println(testVariableNewWay)
 }
